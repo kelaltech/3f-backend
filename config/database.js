1a615dbd7f14ea8845c651dd59aef6678b1cb8d4
dagem <dagixmeko@gmail.com>
2021-06-22 20:20:23 +0300

    database stuff

--- config/database.js
+++ config/database.js
@@ -1,20 +1,42 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', '3f-backend'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'Pass1234'),
-        ssl:{ 
-          rejectUnauthorized: false
+require('./functions/load-env')()
+
+const { ConnectionString } = require('connection-string')
+
+module.exports = ({ env }) => {
+  const parsed = new ConnectionString(
+    env(
+      'DATABASE_URL',
+      `postgres://${env('DATABASE_USERNAME', 'postgres')}:${env(
+        'DATABASE_PASSWORD',
+        'Pass1234'
+      )}@${env('DATABASE_HOST', '127.0.0.1')}:${env.int(
+        'DATABASE_PORT',
+        5432
+      )}/${env('DATABASE_NAME', '3f-backend')}?ssl=${env.bool(
+        'DATABASE_SSL',
+        false
+      )}`
+    )
+  )
+
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: parsed.protocol,
+          username: parsed.user,
+          password: parsed.password,
+          host: parsed.hostname,
+          port: parsed.port,
+          database: parsed.path.join('/'),
+          ssl: {
+            rejectUnauthorized: parsed.params && parsed.params.ssl === 'true', // For self-signed certificates
+          },
         },
+        options: {},
       },
-      options: {}
     },
-  },
-});
+  }
+}
