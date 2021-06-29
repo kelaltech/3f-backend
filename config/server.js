62d2fb8c099dc05ab56c125522a22550db881f0f
dagem <dagixmeko@gmail.com>
2021-06-29 11:13:24 +0300

    JWT for strapi login

--- config/server.js
+++ config/server.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: 'a86b9aa8-43ce-4c17-9308-d8981d3fda3a'
+    }
+  }
 });
